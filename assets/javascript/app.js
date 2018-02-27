6ee1f16097a23916b31bf161da33c07dc57b15fd
DanFein <danfeinstat@gmail.com>
2018-02-26 17:19:39 -0800

    got base ajax call functioning, added animate.css library, added cardback images, working on html format

---
+++ assets/javascript/app.js
@@ -0,0 +1,39 @@
+// $(document).ready(function(){
+//
+//
+//     let spellName = "Aid";
+//     let queryURL = "http://www.dnd5eapi.co/api/spells"
+//
+//     //call for the spell list
+//     $.ajax({
+//       url: queryURL,
+//       method:"GET"
+//     }).then(function(response){
+//
+//       //iterate through spell list for the correct spell and url
+//       for(var i = 0; i<response.results.length; i++){
+//         if(response.results[i].name == spellName){
+//
+//           //call for the specific spell
+//           $.ajax({
+//             url:response.results[i].url,
+//             method:"GET"
+//
+//           //plug in the relevant spell data
+//           }).then(function(data){
+//             $("#name").append(data.name);
+//             $("#range").append(data.range);
+//             $("#duration").append(data.duration);
+//             $("#material").append(data.material);
+//             $("#ritual").append(data.ritual);
+//             $("#level").append(data.level);
+//             $("#desc").append(data.desc);
+//             $("#higher_level").append(data.higher_level);
+//             $("#school").append(data.school.name);
+//             $("#concentration").append(data.concentration);
+//           })//second then statement closes
+//         } //if statement closes
+//       }//for loop closes
+//     })//first then statement closes
+//
+// })//document.ready closes
