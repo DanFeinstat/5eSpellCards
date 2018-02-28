e51c1a51ba3098f208a1d148e0d66e0812142ec5
DanFein <danfeinstat@gmail.com>
2018-02-28 14:35:44 -0800

    designed .back of card to hold content and connected ajax call to spell name submition to populate the spell card

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Generates digital spell cards for your 5e d&amp;d game.
 To-do list:
   store spell selection to local storage;
 
-  add specific spell selection input form (mystical tutor) on class selection.  Use boolean to make sure a spell is selected before card flip is available; -- Partially complete, needs animation debug and to accept input;
+  add specific spell selection input form (mystical tutor) on class selection.  Use boolean to make sure a spell is selected before card flip is available;
+    -- Partially complete, needs
+        - animation debug and to
+        - only accept input once
+        -maybe change to auto flip on spell entry
 
   Add 'please select a spell' modal and 'could not find that spell' modal for for fail cases;
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
   let cardNum = 0;
   let classSelection = $('#classContainer').html();
+
     //click on class button to bring up correct cardback
     $('#classContainer').on('click','.classBtn',function(){
       cardNum ++;
@@ -9,10 +10,48 @@ $(document).ready(function(){
       $('#cardDisplay_Main').append('<div class="card currentCard animated bounceInRight">\
         <div id="flipper">\
         <img  id="card_'+cardNum+'_img" class="card-img" src="'+cardClass+'" alt="bard card">\
-        </div></div>' ).one('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationend',function(){
+        </div></div>' ).one(
+
+          //clear the animate classes
+          //add necessary content page for card flip
+          'webkitAnimationEnd mozAnimationEnd oAnimationEnd animationend',function(){
           $('.currentCard').removeClass('animated bounceInRight');
           $('#card_'+cardNum+'_img').attr('class','card-img front');
-          $('#flipper').append('<div class="back bg-primary"></div>');
+          $('#flipper').append('<div class="back mx-auto">\
+              <div class="nameDisplay text-center"><h6 class="name"></h6></div>\
+              <div class="materialsDisplay">\
+                <p class="materials mx-2"><p>\
+              </div>\
+              <div class="cardDisplaySpace text-center float-left"><h6 class="mt-1">RANGE</h6>\
+                <p class="range"></p>\
+              </div>\
+              <div class="cardDisplaySpace text-center float-left"><h6 class="mt-1">DURATION</h6>\
+                <p class="duration"></p>\
+              </div>\
+              <div class="cardDisplaySpace text-center float-left"><h6 class="mt-1">CASTING TIME</h6>\
+                <p class="castingTime"></p>\
+              </div>\
+              <div class="cardDisplaySpace text-center float-left"><h6 class="mt-1">COMPONENTS</h6>\
+                <p class="components"></p>\
+              </div>\
+              <div class="cardDescDisplay mb-2"><h6 class="mt-1 mb-1 text-center">SPELL DESCRIPTION</h6>\
+                <p class="desc mx-2"></p>\
+                <p class="higher_level mx-2"></p>\
+              </div>\
+          \
+              <div class="row">\
+                <div class="col-1">\
+                </div>\
+                <div class="schoolDipslay col-5 text-center"><h6 class="school"></h6>\
+                </div>\
+                <div class="ritualDisplay col-5">\
+                  <h6 class="isRitual text-center">R: <span class="ritual"></span></h6>\
+                </div>\
+              </div>\
+            </div>');
+
+          //add event on end of cardback entry animation
+          //Class buttons animated leaving and are then replaced with spell selection option
           $('#classContainer').attr('class', 'animated zoomOutUp').one('webkitAnimationEnd mozAnimationEnd oAnimationEnd animationend',function(){
             $('#classContainer').empty();
             $('#classContainer').removeClass('animated zoomOutUp');
@@ -22,52 +61,83 @@ $(document).ready(function(){
                     <label for="spellNameInput">Mystical Tutor</label>\
                       <input type="text" class="form-control" id="spellNameInput" placeholder="Enter Spell Name Here">\
                   </div>\
-                  <button type="submit" class="btn btn-dark">Submit</button>\
+                  <button class="btn btn-dark submitSpell">Submit</button>\
                 </form>');
           })//form entry .one animation closes
         })//current card .one event closes
       })//onclick even closes
 
+    //on click of current card it transforms to the content ('.back') div
     $('#cardDisplay_Main').on('click','.currentCard',function(){
       $('#flipper').attr('class', 'flipper');
     })
 
 
 
+
+    //accept input from mystical tutor form
+    //use it to search for the spell from the api
+    //populate data on the card
+    $('#classContainer').on('click','.submitSpell',function(event){
+      event.preventDefault();
+
+      let spellName = $('#spellNameInput').val().trim();
+      let queryURL = "http://www.dnd5eapi.co/api/spells"
+
+      String.prototype.toTitleCase = function() {
+        var i, j, str, lowers, uppers;
+        str = this.replace(/([^\W_]+[^\s-]*) */g, function(txt) {
+          return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+        });
+
+        // Certain minor words should be left lowercase unless
+        // they are the first or last words in the string
+        lowers = ['A', 'An', 'The', 'And', 'But', 'Or', 'For', 'Nor', 'As', 'At',
+        'By', 'For', 'From', 'In', 'Into', 'Near', 'Of', 'On', 'Onto', 'To', 'With'];
+        for (i = 0, j = lowers.length; i < j; i++)
+          str = str.replace(new RegExp('\\s' + lowers[i] + '\\s', 'g'),
+            function(txt) {
+              return txt.toLowerCase();
+            });
+
+            return str;
+        }
+        spellName = spellName.toTitleCase();
+        console.log(spellName);
+      //call for the spell list
+      $.ajax({
+        url: queryURL,
+        method:"GET"
+      }).then(function(response){
+
+        //iterate through spell list for the correct spell and url
+        for(var i = 0; i<response.results.length; i++){
+          if(response.results[i].name == spellName){
+
+            //call for the specific spell
+            $.ajax({
+              url:response.results[i].url,
+              method:"GET"
+
+            //plug in the relevant spell data
+            }).then(function(data){
+              $(".name").append(data.name);
+              $(".range").append(data.range);
+              $(".duration").append(data.duration);
+              $(".materials").append(data.material);
+              $(".ritual").append(data.ritual);
+              $(".components").append(data.components);
+              $(".desc").append(data.desc);
+              $(".higher_level").append(data.higher_level);
+              $(".school").append(data.school.name);
+              $(".castingTime").append(data.casting_time);
+            })//second then statement closes
+          } //if statement closes
+        }//for loop closes
+      })//first then statement closes
+    })//on click submit event closes
+
 })//document.ready closes
 
 //
-// let spellName = "Aid";
-// let queryURL = "http://www.dnd5eapi.co/api/spells"
-//
-// //call for the spell list
-// $.ajax({
-//   url: queryURL,
-//   method:"GET"
-// }).then(function(response){
-//
-//   //iterate through spell list for the correct spell and url
-//   for(var i = 0; i<response.results.length; i++){
-//     if(response.results[i].name == spellName){
-//
-//       //call for the specific spell
-//       $.ajax({
-//         url:response.results[i].url,
-//         method:"GET"
-//
-//       //plug in the relevant spell data
-//       }).then(function(data){
-//         $("#name").append(data.name);
-//         $("#range").append(data.range);
-//         $("#duration").append(data.duration);
-//         $("#material").append(data.material);
-//         $("#ritual").append(data.ritual);
-//         $("#level").append(data.level);
-//         $("#desc").append(data.desc);
-//         $("#higher_level").append(data.higher_level);
-//         $("#school").append(data.school.name);
-//         $("#concentration").append(data.concentration);
-//       })//second then statement closes
-//     } //if statement closes
-//   }//for loop closes
-// })//first then statement closes
+// let spellName = "";
